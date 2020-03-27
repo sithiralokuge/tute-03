8de7edb4c1e67fdd9a8d5b903c074ccb292ef7b0
sithiralokuge <61415982+sithiralokuge@users.noreply.github.com>
2020-03-27 11:52:03 -0700

    tute 03

---
+++ app/src/main/java/com/example/androidfragments/MainActivity.java
@@ -0,0 +1,44 @@
+
+
+package com.example.androidfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view) {
+        Fragment fragment;
+
+        if (view == findViewById(R.id.btnFragment1)) {
+            fragment = new Fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefaul, fragment);
+            ft.commit();
+        }
+
+        if (view == findViewById(R.id.btnFragmetn2)) {
+            fragment = new Fragment2();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefaul, fragment);
+            ft.commit();
+        }
+
+    }
+
+}
